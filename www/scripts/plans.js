a6bcb1dd3f15beefc24d1e806942e5db1bf1eeac
Jakub Szkarłat <jakubszkarlat@gmail.com>
2018-03-31 15:00:39 +0200

    Added basic plans layout

---
+++ www/scripts/plans.js
@@ -0,0 +1,30 @@
+const tabButtonClass = '.tab-button';
+const tabClass = '.tab';
+const tabButtons = document.querySelectorAll(tabButtonClass);
+const tabs = document.querySelectorAll(tabClass);
+
+const activate = (element, elementClass) => {
+    element.classList.add('active');
+
+    // console.log(element, elementClass);
+    if (elementClass !== undefined) {
+        const elementArray = [...document.querySelectorAll(elementClass)];
+
+        elementArray.forEach((value) => {
+            if (value !== element && value.classList.contains('active'))
+                value.classList.remove('active');
+        });
+    }
+};
+
+tabButtons.forEach((tabButton, index) => {
+   tabButton.addEventListener('click', (event) => {
+       let element = event.target;
+
+       activate(element, tabButtonClass);
+       activate(tabs[index], tabClass)
+   });
+});
+
+activate(tabButtons[0], tabButtonClass);
+activate(tabs[0], tabClass);
