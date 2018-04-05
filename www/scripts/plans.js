6dfa4248de7c73e8673954c91794696ab9e0a1c8
Jakub Szkarłat <jakubszkarlat@gmail.com>
2018-04-05 22:18:43 +0200

    Moved planItem templating functions to different file

--- www/scripts/plans.js
+++ www/scripts/plans.js
@@ -1,26 +1,3 @@
-const getPlanItemTemplate = () => {
-    const planItemTemplate = document.querySelector('#plan-item-template');
-    planItemTemplate.remove();
-    planItemTemplate.removeAttribute('id');
-    return planItemTemplate.cloneNode(true);
-};
-
-const insertPlanItemTemplate = (data, item, container, type) => {
-    const planItem = item.cloneNode(true);
-    const saveButton = planItem.querySelector('.plan-item-controls').children[0];
-    const name = planItem.querySelector('.plan-item-name');
-
-    if (type !== undefined)
-        planItem.classList.add(`type-${type}`);
-
-    name.innerHTML = data.name;
-    saveButton.addEventListener('click', () => {
-        document.querySelector('#search').value = data.id;
-    });
-
-    container.appendChild(planItem);
-};
-
 const planItemTemplate = getPlanItemTemplate();
 
 let planList = document.querySelector('#tab-groups');
