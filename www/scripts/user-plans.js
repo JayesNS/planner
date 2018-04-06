f6010da66d2e2901352fe7088c8a270910d9649c
Jakub Szkarłat <jakubszkarlat@gmail.com>
2018-04-06 18:22:16 +0200

    Added main-plan

--- www/scripts/user-plans.js
+++ www/scripts/user-plans.js
@@ -3,7 +3,7 @@ const container = document.querySelector('main');
 
 const types = ['group', 'teacher', 'classroom'];
 
-for (let i = 0; i < 10; ++i) {
+for (let i = 0; i < 5; ++i) {
     let type = Math.floor(Math.random()*3);
     insertPlanItemTemplate({name: 'grupa'+i, id: i}, planItemTemplate, container, types[type]);
 }
