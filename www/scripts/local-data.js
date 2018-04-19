f229ac6ba6b5dbc88b416c991d958726347a93d8
Jakub Szkarłat <jakubszkarlat@gmail.com>
2018-04-19 14:59:45 +0200

    Refactored local-data script

--- www/scripts/local-data.js
+++ www/scripts/local-data.js
@@ -2,33 +2,45 @@ const localStorageKeys = {
     'LOCAL_PLANS': 'local-plans'
 };
 
+// Setting plan to local storage
 const setLocalPlan = (plan) => {
-    if (!localStorage.getItem(localStorageKeys.LOCAL_PLANS))
-        return;
-
-    let localPlans = JSON.parse(localStorage.getItem(localStorageKeys.LOCAL_PLANS));
-
-    localPlans[plan['nazwa']] = plan;
-    localStorage.setItem(localStorageKeys.LOCAL_PLANS, JSON.stringify(localPlans));
-};
-
-const getLocalPlans = (planName) => {
-    if (!localStorage.getItem(localStorageKeys.LOCAL_PLANS))
-        return;
-
-    if (planName)
-        return JSON.parse(localStorage.getItem(localStorageKeys.LOCAL_PLANS))[planName];
-
-    return JSON.parse(localStorage.getItem(localStorageKeys.LOCAL_PLANS));
-};
-
-const removeLocalPlan = (planName) => {
-    if (!localStorage.getItem(localStorageKeys.LOCAL_PLANS))
-        return;
-
+    // Obtaining local plan list
     let localPlans = getLocalPlans();
-    delete localPlans[planName];
-    localStorage.setItem(localStorageKeys.LOCAL_PLANS, JSON.stringify(localPlans));
 
-    console.log(planName, localPlans);
+    // Appending new plan to list
+    localPlans[plan['nazwa']] = plan;
+
+    // Saving updated list to localStorage
+    localStorage.setItem(localStorageKeys.LOCAL_PLANS, JSON.stringify(localPlans));
+};
+
+// Getting specified plan or all plans if parameter not given
+const getLocalPlans = (planName) => {
+    const localPlans = localStorage.getItem(localStorageKeys.LOCAL_PLANS);
+
+    // Checking if local plans exist
+    if (!localPlans)
+        return;
+
+    // If planName is given and if it exists in localStorage
+    if (planName && localPlans[planName])
+        return JSON.parse(localPlans)[planName];
+
+    // If parameter is not given return all local plans
+    return JSON.parse(localPlans);
+};
+
+// Remove plan with given name
+const removeLocalPlan = (planName) => {
+    let localPlans = getLocalPlans();
+
+    // Checking if local plans exist
+    if (!localPlans)
+        return;
+
+    // Deleting plan with given name
+    delete localPlans[planName];
+
+    // Setting updated plan list without removed plan
+    localStorage.setItem(localStorageKeys.LOCAL_PLANS, JSON.stringify(localPlans));
 };
