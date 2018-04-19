37f63e63df19edcf729b593a452bcce25b85ad23
Jakub Szkarłat <jakubszkarlat@gmail.com>
2018-04-19 14:31:45 +0200

    Refactoring remote data script

--- www/scripts/remote-data.js
+++ www/scripts/remote-data.js
@@ -1,23 +1,34 @@
+// Obtaining data from remote server and returning Promise
 const getData = (url) => {
     return new Promise((resolve, reject) => {
+
         let xhr = new XMLHttpRequest();
+
         xhr.open('GET', url, true);
+
         xhr.onreadystatechange = () => {
+            // Checking if request is done
             if (xhr.readyState === 4) {
-                if(xhr.status === 200)
+                if (xhr.status === 200) {
                     resolve(JSON.parse(xhr.responseText));
-                else
-                    reject("Błąd podczas ładowania strony\n");
+                }
+
+                reject("Błąd podczas ładowania strony");
             }
         };
         xhr.send();
     });
 };
 
+// Making API request url from given parameters
 const prepareUrl = (type, id, range) => {
+    // Base url
     let url = 'http://api.jsthats.me/api';
-    url += type !== undefined ? '/'+type : '';
-    url += id !== undefined ? '/'+id : '';
-    url += range !== undefined ? '/'+range : '';
+
+    // Appending values to url if they exist
+    url += type ? '/'+type : '';
+    url += id ? '/'+id : '';
+    url += range ? '/'+range : '';
+
     return url;
 };
